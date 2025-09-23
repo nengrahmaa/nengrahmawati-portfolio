92cb0e12ebfe0c6a41ae9f502076a7fa73dd4ec2
nengrahmaa <nengamaa01@gmail.com>
2025-09-23 15:41:44 +0700

    Fix bug

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import {} from "framer-motion";
+import {motion} from "framer-motion";
 import certificates from "../data/certificates.json";
 import CertificateCard from "../components/CertificateCard";
 import CertificateModal from "../components/CertificateModal";
